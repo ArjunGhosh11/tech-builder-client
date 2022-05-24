716374e1382704d31131f95ca5bfcba204c3f784
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-24 12:52:22 +0600

    All routes set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Purchase from './Pages/Purchase/Purchase';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import MyProfile from './Pages/Dashboard/MyProfile';
+import AddReview from './Pages/Dashboard/AddReview';
+import AddProduct from './Pages/Dashboard/AddProduct';
+import MyOrders from './Pages/Dashboard/MyOrders';
+import MakeAdmin from './Pages/Dashboard/MakeAdmin';
+import ManageAllOrders from './Pages/Dashboard/ManageAllOrders';
+import ManageProducts from './Pages/Dashboard/ManageProducts';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp';
+import Blogs from './Pages/Blogs/Blogs';
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -11,8 +24,22 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
-        <Route path="" element={<Home />} />
+        <Route path="/purchase" element={<Purchase />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<MyProfile></MyProfile>}></Route>
+          <Route path='addReview' element={<AddReview></AddReview>}></Route>
+          <Route path='addProduct' element={<AddProduct></AddProduct>}></Route>
+          <Route path='myOrders' element={<MyOrders></MyOrders>}></Route>
+          <Route path='makeAdmin' element={<MakeAdmin></MakeAdmin>}></Route>
+          <Route path='manageProducts' element={<ManageProducts></ManageProducts>}></Route>
+          <Route path='manageAllOrders' element={<ManageAllOrders></ManageAllOrders>}></Route>
+        </Route>
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="myPortfolio" element={<MyPortfolio />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
