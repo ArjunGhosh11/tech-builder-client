a29e5b2ca5de37d22420bbf92c3ebba9f38fb1cd
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-26 00:00:37 +0600

    Readme blog and portfolio added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>Blogs</h2>
+            <h2 className='text-center text-3xl font-bold text-accent' >BLOGS</h2>
+            <div className='p-20'>
+                <h2 className='font-bold text-lg'>14.1 How will you improve the performance of a React Application?</h2>
+                <p>=</p>
+                <h2 className='font-bold text-lg'>14.2 What are the different ways to manage a state in a React application?</h2>
+                <p>=</p>
+                <h2 className='font-bold text-lg'>14.3 How does prototypical inheritance work?</h2>
+                <p>=</p>
+                <h2 className='font-bold text-lg'>14.4 Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProduct</h2>
+                <p>=</p>
+                <h2 className='font-bold text-lg'>14.5 You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h2>
+                <p>=</p>
+                <h2 className='font-bold text-lg'>14.6 What is a unit test? Why should write unit tests?</h2>
+                <p>=</p>
+            </div>
         </div>
     );
 };
--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -4,7 +4,41 @@ import Loading from '../Shared/Loading';
 const MyPortfolio = () => {
     return (
         <div>
-            <h2>My Portfolio</h2>
+            <h2 className='text-center text-3xl font-bold text-accent'>MY PORTFOLIO</h2>
+            <div className='p-10 '>
+                <div className='bg-neutral text-white text-bold p-10 rounded-3xl'>
+                    <ul>
+                        <li><h2 className='text-2xl mb-3'>NAME: ARJUN GHOSH</h2></li>
+                        <li><h2 className='text-2xl mb-3'>EMAIL: ghosh.rj11@gmail.com</h2></li>
+                        <li><h2 className='text-2xl mb-3'>PRIMARY EDUCATION: DAFFODIL INTERNATIONAL SCHOOL, TANGAIL</h2></li>
+                        <li><h2 className='text-2xl mb-3'>SECONDARY EDUCATION: ST. MICHAEL'S SCHOOL, SILIGURI (ICSE,2019(passed with 96%))</h2></li>
+                        <li><h2 className='text-2xl mb-3'>HIGHER SECONDARY EDUCATION: THE MODERN ACADEMY, KOLKATA(ISC,2021(passed with 94%))</h2></li>
+                        <li><h2 className='text-2xl mb-3'>CURRENTLY(UNDERGRADUATE): BRAC UNIVERSITY, DHAKA (CSE DEPARTMENT)</h2></li>
+                    </ul>
+                    <hr />
+                    <div className='font-bold'>
+                        <h2 className='text-xl my-3'>LIST OF TECHNOLOGIES</h2>
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>Bootstrap</p>
+                        <p>Tailwind</p>
+                        <p>React Router</p>
+                        <p>React Firebase Hooks</p>
+                        <p>Firebase</p>
+                        <p>Stripe</p>
+                        <p>Mongodb</p>
+                        <p className='mb-3'>Nodejs</p>
+                    </div>
+                    <hr />
+                    <div className='flex flex-col font-bold '>
+                        <h2 className='text-xl my-3'>LIST OF WEBSITES</h2>
+                        <a className='text-accent' target='_blank' href="https://tech-builder-49d52.web.app/">TECH BUILDER</a>
+                        <a className='text-accent' target='_blank' href="https://assignment-11-281bf.firebaseapp.com/">TECH DOWN</a>
+                        <a className='text-accent' target='_blank' href="https://assignment-10-f82c0.web.app/">MEMORIES</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
